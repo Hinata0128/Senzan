16c6a1462cb8529c4c492d268f0e3e7075a8150d
hinat <hinat@>
2026-03-05 12:25:48 +0900

    雪玉を作成していく

--- Senzan/SourceCode/Game/01_GameObject/00_MeshObject/00_Character/03_SnowBall/Ball.cpp
+++ Senzan/SourceCode/Game/01_GameObject/00_MeshObject/00_Character/03_SnowBall/Ball.cpp
@@ -2,7 +2,26 @@
 
 Ball::Ball()
 	: MeshObject()
-	, m_IsParried(false)
+    , ThrowingTime(0.0f)
+    , Totle_ThrowingTime(1.5f)
+    , m_upColliders(std::make_unique<CompositeCollider>())
+    , m_pAttackCollider(nullptr)
+    , m_ParriedAnimTime(0.0f)
+    , m_State(enSnowBall::Idol)
+    , m_IsParried(false)
+    , m_IsHitAnimPlaying(false)
+    , m_IsFalling(false)
+    , m_FallSpeed(50.0f)
+    , m_IsLanded(false)
+    , m_FadeDuration(2.0f)
+    , m_FadeTimer(0.0f)
+    , m_GroundY(1.f)
+    , m_ShouldDestroy(false)
+    , m_HasBrokenVisual(false)
+    , m_BounceSpeed(60.0f)
+    , m_ParryStartPos({ 0.0f,0.0f,0.0f })
+    , m_ParryElapsed(0.0f)
+    , m_ParryDuration(1.0f)
 {
 }
 
